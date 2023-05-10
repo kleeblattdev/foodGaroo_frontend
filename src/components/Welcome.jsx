27c6b99f7bd61e90b2c69be48e348e62948261d4
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-10 21:33:37 +0200

    added animation to splash screen

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,12 +3,10 @@ import SquareButtonLight from "../shared/buttons/SquareButtonLight";
 import SquareButton from "../shared/buttons/SquareButton";
 
 import "./welcome.scss";
-import Searchbar from "../shared/Searchbar";
 
 const Welcome = () => {
 	return (
 		<main className="welcome">
-			<Searchbar />
 			<Logo />
 			<h2>Welcome to FoodGaroo</h2>
 			<p>Let FoodGaroo hop to it and bring your groceries to you!</p>
