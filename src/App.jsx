50a4a4719b6a5487bfe9bd0bd4e008d367d640b7
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-10 14:11:42 +0200

    onBoarding with timeout

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import OnBoarding from "./pages/OnBoarding";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import RegisterSuccess from "./pages/RegisterSuccess";
+import Welcome from "./components/Welcome";
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<OnBoarding />} />
+					<Route path="/welcome" element={<Welcome />} />
 					<Route path="/register" element={<Register />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/successRegistration" element={<RegisterSuccess />} />
