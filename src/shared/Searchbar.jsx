c921bf20dc71b6e0b1d30cfe94ae824312954299
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-10 21:12:18 +0200

    searchbar styling

--- src/shared/Searchbar.jsx
+++ src/shared/Searchbar.jsx
@@ -1,7 +1,9 @@
+import "./searchbar.scss";
+
 const Searchbar = () => {
 	return (
 		<section className="searchbar">
-			<input type="text" />
+			<input type="text" placeholder="Search for product" />
 		</section>
 	);
 };
