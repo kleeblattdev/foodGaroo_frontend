8ba12a183140ef1a04bacb259844ca982b98cecf
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-05-11 22:07:08 +0200

    fE filter SortBy geht nur einen auszuwählen + Slider

--- src/pages/Filter.jsx
+++ src/pages/Filter.jsx
@@ -1,21 +1,41 @@
-import React, { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import MultiRangeSlider from "multi-range-slider-react";
-
-
-
-
-
+import { useNavigate } from "react-router-dom";
+import SquareButtonLight from "../shared/buttons/SquareButtonLight";
 
 
 const Filter = () => {
+
+	const nav = useNavigate();
+	const url = import.meta.env.VITE_BACKEND + import.meta.env.VITE_API_VERSION
 
 
 	const [minValue, set_minValue] = useState(25);
 	const [maxValue, set_maxValue] = useState(400);
 	const handleInput = (e) => {
 		set_minValue(e.minValue);
+		setPriceFrom(e.minValue);
 		set_maxValue(e.maxValue);
+		setPriceTo(e.maxValue);
 	};
+
+	const [aktivButton, setAktivButton] = useState(false);
+	// lowest, highest, newest, likes, spoonacularScore
+
+	const [priceFrom, setPriceFrom] = useState(0);
+	const [priceTo, setPriceTo] = useState(500);
+	// inputs von values von slider
+	console.log(priceFrom, priceTo)
+
+	const [sortBy, setSortBy] = useState();
+	// lowest, highest, newest, likes, spoonacularScore
+	console.log(sortBy)
+
+
+	const handelInputToFetch = async (e) => {
+		e.preventDefault();
+	}
+
 
 
 
@@ -23,64 +43,84 @@ const Filter = () => {
 		<main className="filter">
 			<h1>Filter</h1>
 
-			<div className="App">
-				<MultiRangeSlider
-					min={0}
-					max={500}
-					step={5}
-					minValue={minValue}
-					maxValue={maxValue}
-					onInput={(e) => {
-						handleInput(e);
-					}}
-				/>
-			</div>
+			<form onSubmit={handelInputToFetch}>
 
 
-			<section>
-				<h2>Sort By:</h2>
-				<button>Lowest</button>
-				<button>Highest</button>
-				<button>Newest</button>
-			</section>
+				<section>
+					<h2>Sort By: nur einer geht zum auswählen </h2>
 
-			<section>
-				<h2>Price:</h2>
-				<form action="">
-					<p></p>
-					<label htmlFor="priceFrom">min</label>
-					<input type='number' name="priceFrom" placeholder="0" />
-					<p>-</p>
-					<input type="number" name="priceTo" placeholder="500" />
-					<label htmlFor="priceTo">max</label>
-				</form>
-			</section>
+					<button onClick={() => { setAktivButton('lowest'), setSortBy('lowest') }}
+						style={(aktivButton === 'lowest') ? { backgroundColor: 'green' } : { backgroundColor: 'grey' }}
 
-			<section>
-				<h2>Category:</h2>{/* category */}
-				<button>Frozen</button>
-				<button>Vegetarian</button>
-				<button>Vegan</button>
+					>Lowest</button>
 
-			</section>
+					<button onClick={() => { setAktivButton('highest'), sortBy('highest') }}
+						style={(aktivButton === 'highest') ? { backgroundColor: 'green' } : { backgroundColor: 'grey' }}
+					>Highest</button>
 
-			<section>
-				<h2>Inhaltsstoffe:</h2> {/* badges */}
-				<button>egg_free</button>
-				<button>peanut_free</button>
-				<button>sugar_free</button>
+					<button onClick={() => { setAktivButton('newest'), sortBy('newest') }}
+						style={(aktivButton === 'newest') ? { backgroundColor: 'green' } : { backgroundColor: 'grey' }}
+					>Newest</button>
+
+					<button onClick={() => { setAktivButton('likes'), sortBy('likes') }}
+						style={(aktivButton === 'likes') ? { backgroundColor: 'green' } : { backgroundColor: 'grey' }}
+					>Likes</button>
+
+					<button onClick={() => { setAktivButton('sponnacularScore') , sortBy('sponnacularScore') }}
+						style={(aktivButton === 'sponnacularScore') ? { backgroundColor: 'green' } : { backgroundColor: 'grey' }}
+					>sponnacular Score</button>
+				</section>
+
+				<section>
+					<h2>Price:</h2>
+
+					<article style={{ width: "50vw" }} className="slider">
+
+						<MultiRangeSlider style={{ borderRadius: '30px', backgroundColor: 'orange' }} className="slider"
+							min={0}
+							max={500}
+							step={10}
+							minValue={minValue}
+							maxValue={maxValue}
+							onInput={(e) => {
+								handleInput(e);
+							}}
+						/>
+					</article>
 
 
-			</section>
+				</section>
 
-			<section>
-				<h2>Allergen:</h2>
-				<button>organic</button>
-				<button>kosher</button>
-				<button>no_artificial_flavors</button>
+				<section>
+					<h2>Category: mehrere auswählen </h2>{/* category */}
+					<button>Frozen</button>
+					<button>Vegetarian</button>
+					<button>Vegan</button>
 
-			</section>
+				</section>
 
+				<section>
+					<h2>Inhaltsstoffe: mehrere auswählen</h2> {/* badges */}
+					<button>egg_free</button>
+					<button>peanut_free</button>
+					<button>sugar_free</button>
+
+
+				</section>
+
+				<section>
+					<h2>Allergen: mehrere auswählen</h2>
+					<button>organic</button>
+					<button>kosher</button>
+					<button>no_artificial_flavors</button>
+
+
+				</section>
+				<section  >
+					<SquareButtonLight type='submit'
+						style={{ margin: "200px", color: 'red' }}  >Apply</SquareButtonLight>
+				</section>
+			</form>
 		</main>
 	);
 };
