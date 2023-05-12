740744407c6fd3eaf52cc0a6d5f627911bc5a406
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-12 09:10:09 +0200

    installed uuid

--- src/shared/Searchbar.jsx
+++ src/shared/Searchbar.jsx
@@ -1,16 +1,21 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./searchbar.scss";
 
 const Searchbar = () => {
 	const url = import.meta.env.VITE_BACKEND + import.meta.env.VITE_API_VERSION;
 	const searchRef = useRef();
+	const [searchItem, setSearchItem] = useState();
 
 	const handleSearch = () => {
 		fetch(
 			url + "/search?search=" + searchRef.current.value + "&offset=0&limit=20"
 		)
 			.then((res) => res.json())
-			.then((data) => console.log(data));
+			.then((data) => {
+				setSearchItem(data);
+				console.log(data);
+			});
 	};
 
 	return (
@@ -21,6 +26,8 @@ const Searchbar = () => {
 				ref={searchRef}
 				onChange={handleSearch}
 			/>
+			{/* 			{searchItem && searchItem.map((item)=>{
+			})} */}
 		</section>
 	);
 };
