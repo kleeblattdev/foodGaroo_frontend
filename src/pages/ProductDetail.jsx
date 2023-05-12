f3aa7bd4b9ddcb2b0aa5ce70b10a03bad34ce8f6
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-12 15:42:33 +0200

    deleted css and scss files, added productDetail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -12,6 +12,8 @@ const ProductDetail = () => {
 	const url = import.meta.env.VITE_BACKEND + import.meta.env.VITE_API_VERSION;
 
 	const [product, setProduct] = useState();
+	const [description, setDescription] = useState(false);
+	const [ingredient, setIngredient] = useState(false);
 	const [quantity, setQuantity] = useState(1);
 
 	useEffect(() => {
@@ -23,31 +25,59 @@ const ProductDetail = () => {
 			});
 	}, []);
 
+	const handleDescription = () => {
+		if (description == true) {
+			return setDescription(false);
+		}
+		return setDescription(true);
+	};
+
+	const handleIngredient = () => {
+		if (ingredient == true) {
+			return setIngredient(false);
+		}
+		return setIngredient(true);
+	};
+
 	const handleAddToCart = async () => {};
 
 	return (
 		<main className="productDetail">
 			<Header></Header>
-			<section className="product">
-				<img src={product?.image} alt={product?.title} />
-				<h3>{product?.price}€</h3>
-				<h4>
-					{product?.servings.size}
-					{product?.servings.unit}
-				</h4>
-			</section>
-			<section className="setQuantity">
-				<h5>Quantity</h5>
-				<article>
-					<button onClick={() => setQuantity(quantity - 1)}>-</button>
-					<h3>{quantity}</h3>
-					<button onClick={() => setQuantity(quantity + 1)}>+</button>
+			<section className="productWrapper">
+				<section className="product">
+					<img src={product?.image} alt={product?.title} />
+					<h3>{product?.price}€</h3>
+					<h4>
+						{product?.servings.size}
+						{product?.servings.unit}
+					</h4>
+				</section>
+				<article className="description">
+					<button onClick={handleDescription}>Description</button>
+					<p className={description ? "showDescription" : "hideDescription"}>
+						{product?.description}
+					</p>
 				</article>
+				<article className="description">
+					<button onClick={handleIngredient}>Ingredients</button>
+					<p className={ingredient ? "showDescription" : "hideDescription"}>
+						{product?.ingredientList}
+					</p>
+				</article>
+				<section className="setQuantity">
+					<h5>Quantity</h5>
+					<article>
+						<button onClick={() => setQuantity(quantity - 1)}>-</button>
+						<h3>{quantity}</h3>
+						<button onClick={() => setQuantity(quantity + 1)}>+</button>
+					</article>
+				</section>
+				<button onClick={handleAddToCart} id="addToCartBtn">
+					Add to Cart
+				</button>
+				<Navigation />
 			</section>
-			<button onClick={handleAddToCart} id="addToCartBtn">
-				Add to Cart
-			</button>
-			<Navigation />
 		</main>
 	);
 };
