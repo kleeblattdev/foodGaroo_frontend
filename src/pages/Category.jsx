b5a6186506e39b58accf8fa745a08b98e9df033c
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-15 11:21:53 +0200

    productitem styling

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,7 @@
 import Header from "../shared/Header";
 import Searchbar from "../shared/Searchbar";
 import Navigation from "../shared/Navigation";
+import ProductItem from "../shared/ProductItem";
 import SearchItem from "../components/SearchItem";
 import { v4 as uuidv4 } from "uuid";
 import { useLocation } from "react-router-dom";
@@ -24,17 +25,12 @@ const Category = () => {
 			<CategoryList />
 			<Navigation />
 
-			{location?.state?.searchCount}
-			{location?.state?.searchResult?.map((item) => {
-				return (
-					<SearchItem
-						key={uuidv4()}
-						title={item.title}
-						_id={item._id}
-						item={item}
-					></SearchItem>
-				);
-			})}
+			<h4>Results: {location?.state?.searchCount}</h4>
+			<section className="productList">
+				{location?.state?.searchResult?.map((item) => {
+					return <ProductItem key={uuidv4()} item={item}></ProductItem>;
+				})}
+			</section>
 		</main>
 	);
 };
