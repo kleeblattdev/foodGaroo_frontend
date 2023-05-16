8d8ecc875e811cff470006fbf73d806678fd7a63
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-05-16 21:14:30 +0200

    fE Order Seite grob

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
 import "./orderItem.scss";
 
-const OrderItem = () => {
+const OrderItem = ( {items , order} ) => {
+	console.log(items) // daten kommen an
+	
+	// datum formatieren
+	const schoenesDatum = new Date(order?.date).toLocaleDateString("de-DE")	
+
 	return (
-		<Link to="/orders/:id" className="orderItem">
+		<Link to={`/orders/:${items?._id}`} className="orderItem">
 			<section>
 				<article>
-					<h2>OrderId</h2>
-					<p>Order sum</p>
+					<h2>OrderId: {items?._id} </h2>
+					<p>Order sum: {items?.price.toFixed(2)}</p>
 				</article>
 				<article>
-					<p>payment status</p>
-					<p>status</p>
+					<p>payment status: {order?.payment_status}</p>
+					<p>status: {order?.status}</p>
 					<div>
-						<p>Order date</p>
+						<p>Order date: {schoenesDatum}</p>
 					</div>
 				</article>
 			</section>
--- src/pages/OrderList.jsx
+++ src/pages/OrderList.jsx
@@ -1,14 +1,55 @@
 import OrderItem from "../components/OrderItem";
 import Header from "../shared/Header";
 import Navigation from "../shared/Navigation";
+import { v4 as uuidv4 } from "uuid";
+import { useState, useEffect } from "react";
+
 
 const OrderList = () => {
+
+	const [orderItems, setOrderItems] = useState([])
+	const [order, setOrder] = useState([])
+
+	const url = import.meta.env.VITE_BACKEND + import.meta.env.VITE_API_VERSION
+
+	const getOrder = async () => {
+		try {
+			const response = await fetch(url + '/orders', {
+				method: 'GET',
+				credentials: 'include',
+				headers: { 'content-type': 'application/json' },
+			});
+			if (response.ok) {
+				const data = await response.json()
+				setOrderItems(data[0]?.items)
+				console.log(data?.items)
+				console.log(data[0]?.items[0]?.price)
+				setOrder(data[0])
+			}
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+
+	useEffect(() => {
+		getOrder()
+	}, [])
+
+
 	return (
 		<main className="orderList">
 			<Header>My Orders</Header>
 			<Navigation />
 			<section>
-				<OrderItem />
+				{orderItems?.map((items) => {
+					return	<OrderItem
+						key={uuidv4()}
+						items={items}
+						order={order}
+					> </OrderItem>
+
+				})}
 			</section>
 		</main>
 	);
