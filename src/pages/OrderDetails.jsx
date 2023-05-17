6650da8d66c1a1147c6acc8dcabc2f721fe69827
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-17 12:01:54 +0200

    ordetDetail styling

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -5,17 +5,20 @@ import { v4 as uuidv4 } from "uuid";
 import Header from "../shared/Header";
 import Navigation from "../shared/Navigation";
 
+//scss import
+import "./orderDetails.scss";
+
 const OrderDetails = ({ order, index }) => {
 	const params = useParams();
 	const nav = useNavigate();
-
 	// console.log(order[index]?._id)
 
 	const id = params.id;
-	console.log(id)
+	console.log(id);
+	const orderId = id.slice(0, 10);
 
 	const [orderDetails, setOrderDetails] = useState();
-	console.log(orderDetails)
+	console.log(orderDetails);
 
 	const url = import.meta.env.VITE_BACKEND + import.meta.env.VITE_API_VERSION;
 
@@ -25,68 +28,63 @@ const OrderDetails = ({ order, index }) => {
 				const result = await fetch(url + `/orders/${id}`, {
 					method: "GET",
 					credentials: "include",
-					headers: { 'content-type': 'application/json' },
+					headers: { "content-type": "application/json" },
 				});
 				const data = await result.json();
 				console.log(data);
 				setOrderDetails(data);
-			}
+			};
 			fetchOrderDetails();
-
 		} catch (err) {
 			console.log(err);
 		}
-	}, [])
-
-
+	}, []);
 
 	return (
 		<main className="orderDetails">
-
-
-
-			<Header />
-			<h1>Order Details</h1>
-{/* 			{orderDetails[0]?.items[0]?.price}
- */}			{orderDetails?.map?.((order) => {
+			<Header>Order: {orderId}</Header>
+			{/* 			{orderDetails[0]?.items[0]?.price}
+			 */}{" "}
+			{orderDetails?.map?.((order) => {
 				return (
-					<div 
-					key={uuidv4}>
-						<h1>Order Bestellung</h1>
-						<h3>deals: {order?.deals} %</h3>
-						<h3>sum: {order?.sum} €</h3>
-						<h3>status: {order?.status} </h3>
-						<h3>payment_status: {order?.payment_status}</h3>
-						<h3>dealsDate: {order?.dealsDate }  </h3>
-						<h1>date: {order?.date} </h1>
-					</div>
+					<section key={uuidv4} className="orderInfo">
+						<p>date: {order?.date} </p>
+						{/* <h3>deals: {order?.deals} %</h3> */}
+						<p>sum: {(order?.sum).toFixed(2)} €</p>
+						<p>payment status: {order?.payment_status}</p>
+						<p>status: {order?.status} </p>
+						{/* <h3>dealsDate: {order?.dealsDate} </h3> */}
+					</section>
 				);
 			})}
-
-{/* die einzelnen Bestellungen */}
-			{orderDetails?.map?.((order) => {
-				return order?.items?.map((item) => {
-					return (
-						<div key={uuidv4}>
-							<h1>nächste Item in der Order Bestellung</h1>
-							<h3>titel: {item?.title}</h3>
-							<h3>price: {item?.price}</h3>
-							<p>quantity: {item?.quantity}</p>
-							<p>rabattstatus: {item?.rabattStatus}</p>
-							<p>rabatt: {item?.rabatt} %</p>
-							<p>priceOld: {item?.priceOld}</p>
-							<p>priceNeu: {item?.priceNeu}</p>
-							<p>status: {item?.status}</p>
-							<p>payment_status: {item?.payment_status}</p>
-
-
-						</div>
-					)
-				})
-			}
-
-			)}
-
+			{/* die einzelnen Bestellungen */}
+			<h2>Bought Products:</h2>
+			<section className="orderItemWrapper">
+				{orderDetails?.map?.((order) => {
+					return order?.items?.map((item) => {
+						return (
+							<article key={uuidv4}>
+								<div>
+									<img src={item?.images[0]} alt={item?.title} />
+									<h4>{item?.title}</h4>
+								</div>
+								{/* <h3>price: {(item?.price).toFixed(2)}</h3> */}
+								<div>
+									<p>quantity: {item?.quantity}</p>
+									{/* <p>rabattstatus: {item?.rabattStatus}</p> */}
+									<p>price: {(item?.priceOld).toFixed(2)}€</p>
+								</div>
+								<div>
+									<p>deal: {item?.rabatt} %</p>
+									<p>deal price: {(item?.priceNeu).toFixed(2)}€</p>
+								</div>
+								{/* 								<p>status: {item?.status}</p>
+								<p>payment_status: {item?.payment_status}</p> */}
+							</article>
+						);
+					});
+				})}
+			</section>
 			<Navigation />
 		</main>
 	);
