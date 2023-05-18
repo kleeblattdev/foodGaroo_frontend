0d16b3aabcc4382be4551d6faa87674a8e976fda
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-18 12:38:47 +0200

    cleaned up img folder, changed covor of svgs

--- src/shared/Searchbar.jsx
+++ src/shared/Searchbar.jsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from "react";
-import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import "./searchbar.scss";
 import SearchItem from "../components/SearchItem";
